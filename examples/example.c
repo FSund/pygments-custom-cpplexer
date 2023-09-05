f682856a93a0ad1605bf931dbe4fdea52a6d41cc
b9 <hackerb9@member.fsf.org>
2023-09-04 21:38:42 -0700

    Random code with various keyname subtypes

---
+++ examples/example.c
@@ -0,0 +1,14 @@
+int main(int argc, char *argv[]) {
+    uint64_t register x;
+    time_t tm;
+    const double Tau = M_PI * 2;
+    System header {
+        vec3 p = pos(0.0, 0.0, 0.0);
+        Atom *atom = Atom(p);
+    }
+    xyzzy is a generic keyword;
+    declaration of independence;
+    hello_my_name_is namespace;
+    pseudo podical;
+    reserved and aloof;
+}
