d30ea7a2565b70c72912f93c5dde0ce671918be3
b9 <hackerb9@member.fsf.org>
2023-09-05 22:05:58 -0700

    Shorten to fit on one page with latex

--- examples/example.c
+++ examples/example.c
@@ -1,12 +1,8 @@
-int main(int argc, char *argv[]) {
-    const double Tau = M_PI * 2;
-    System header {
-        vec3 position = vec3(0.0, 0.0, 0.0);
-	Atom *atom = Atom(position);
-    }
-    xyzzy is a generic keyword;
-    declaration of independence;
-    hello_my_name_is namespace;
-    pseudo podical;
-    reserved and aloof;
+const double Tau = M_PI * 2;
+System header {
+    vec3 position = vec3(0.0, 0.0, 0.0);
+    Atom *atom = Atom(position);
 }
+xyzzy is a generic keyword;
+declaration of independence; pseudo podical;
+hello_my_name_is namespace;  reserved and aloof;
