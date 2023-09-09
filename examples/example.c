b8cca15fa19180b60f4340ad46a1f839cbef90c3
b9 <hackerb9@member.fsf.org>
2023-09-08 21:08:29 -0700

    Don't forget to show syntax highling of 'Keyword.Removed'

--- examples/example.c
+++ examples/example.c
@@ -1,6 +1,6 @@
 const double Tau = M_PI * 2;
 vec3 position = vec3(0.0, 0.0, 0.0);
 Atom *atom = Atom(position);
-xyzzy is a generic keyword;
+xyzzy is a generic keyword;  removed stricken;
 declaration of independence; pseudo podical;
 hello_my_name_is namespace;  reserved and aloof;
