83505826bf666813822a83fb60b5eecbd366ade5
hackerb9 <hackerb9@users.noreply.github.com>
2023-09-17 22:05:18 -0700

    Add a test that checks that all the environment variables work as intended (#4)
    * A script to test if env vars are working correctly
    
    * Remove backup file
    
    * Need a different string for a namespace
    
    "using" was already defined in CppLexer.
    
    "Hello, My name is _____" is a NAMESPACE.

---
+++ tests/runtests.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Test that all environment variables are properly used by pygments
+
+export PYGMENTS_CUSTOM_BASE_LEXER=CppLexer
+
+export PYGMENTS_CUSTOM_TYPE="[ 'vec3', 'Atom', 'System' ]"
+export PYGMENTS_CUSTOM_CONSTANT="[ 'M_PI', 'Tau'  ]"
+export PYGMENTS_CUSTOM_KEYWORD="[ 'xyzzy', 'plugh'  ]"
+export PYGMENTS_CUSTOM_DECLARATION="[ 'def', 'fn' ]"
+export PYGMENTS_CUSTOM_NAMESPACE="[ 'hello_my_name_is' ]"
+export PYGMENTS_CUSTOM_PSEUDO="[ 'let' ]"
+export PYGMENTS_CUSTOM_RESERVED="[ 'POKE' ]"
+export PYGMENTS_CUSTOM_REMOVED="[ 'removed' ]"
+
+
+input="new vec3 M_PI xyzzy def hello_my_name_is let POKE removed"
+
+expected="    def testNeedsName(lexer):
+        fragment = 'new vec3 M_PI xyzzy def hello_my_name_is let POKE removed\n'
+        tokens = [
+            (Token.Keyword, 'new'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword.Type, 'vec3'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword.Constant, 'M_PI'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword, 'xyzzy'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword.Declaration, 'def'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword.Namespace, 'hello_my_name_is'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword.Pseudo, 'let'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword.Reserved, 'POKE'),
+            (Token.Text.Whitespace, ' '),
+            (Token.Keyword.Removed, 'removed'),
+            (Token.Text.Whitespace, '\n'),
+        ]
+        assert list(lexer.get_tokens(fragment)) == tokens"
+
+output=$(pygmentize -l custom -f testcase <<<"${input}")
+
+if diff <(echo "$output") <(echo "$expected"); then
+    echo "All environment variables work as expected."
+    exit 0
+else
+    echo "ERROR: Output did not match what was expected."
+    exit 1
+fi
+
